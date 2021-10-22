276c0f1e1fa92728383be6b9a30e4da5ec29f19b
Abhik S Basu <abhiksbasu@Abhiks-MacBook-Air.local>
2021-10-23 03:22:19 +0530

    Finished assignment 3

---
+++ Assignment 3/board.java
@@ -0,0 +1,144 @@
+
+import java.util.Scanner;
+class player{
+    private String name;
+    private int point;
+    private int currFloor=-1;
+    player(String name){
+        this.name=name;
+    }
+    int getPoint(){
+        return this.point;
+    }
+    void setPoint(int a){
+        this.point=a;
+    }
+    String getName(){
+        return this.name;
+    }
+    void setCurrFloor(int floor){
+        this.currFloor=floor;
+    }
+    int getCurrFloor(){
+        return this.currFloor;
+    }
+}
+
+class floor {
+    floor(){};
+    protected void update (player p){
+        System.out.println("Player position Floor - "+p.getCurrFloor());
+        p.setPoint(p.getPoint()+1);
+        System.out.println(p.getName() + " has reached an empty floor");
+        System.out.println("Total Points "+ p.getPoint());
+    }
+    protected void userUpdate(player p,int currFloor, int points){
+        p.setCurrFloor(currFloor);
+        p.setPoint(p.getPoint()+points);
+    }
+}
+class snake extends floor{
+    snake(){};
+    @Override
+    protected void update (player p){
+        System.out.println("Player position Floor - "+p.getCurrFloor());
+        userUpdate(p, 1, -2);
+        System.out.println(p.getName() + " has reached a normal snake floor");
+        System.out.println("Total Points "+ p.getPoint());
+        super.update(p);
+    }
+}
+class ladder extends floor{
+    ladder(){};
+    @Override
+    protected void update (player p){
+        System.out.println("Player position Floor - "+p.getCurrFloor());
+        userUpdate(p, 12, 2);
+        System.out.println(p.getName() + " has reached a ladder floor");
+        System.out.println("Total Points "+ p.getPoint());
+        super.update(p);
+    }
+}
+class kingCobra extends floor{
+    kingCobra(){};
+    @Override
+    protected void update (player p){
+        System.out.println("Player position Floor - "+p.getCurrFloor());
+        userUpdate(p, 3, -4);
+        System.out.println(p.getName() + " has reached a King Cobra floor");
+        System.out.println("Total Points "+ p.getPoint());
+        super.update(p);
+    }
+}
+class elevator extends floor{
+    elevator(){};
+    @Override
+    protected void update (player p){
+        System.out.println("Player position Floor - "+p.getCurrFloor());
+        userUpdate(p, 10, 4);
+        System.out.println(p.getName() + " has reached the elevator floor");
+        System.out.println("Total Points "+ p.getPoint());
+        super.update(p);
+    }
+}
+class dice{
+    private final int numFaces; //maximum face value
+    private int faceValue; 
+    public dice(int numFaces) {
+        this.numFaces = numFaces;
+        roll();
+    }
+    public void roll() {
+    double curr_faceValue = 1 + (Math.random()*2);
+    int faceValue=(int)curr_faceValue;
+    setFaceValue(faceValue);
+    }
+    private void setFaceValue (int value) {
+        if (value <= numFaces)
+            this.faceValue = value;
+    }
+    int getFaceValue(){
+        return this.faceValue;
+    }
+}
+/**
+ * board
+ */
+public class board {
+    public static void main(String[] args) {
+        floor [] floorList= new floor[14];
+        for (int i=0;i<floorList.length;i++){
+            if (!(i==2 || i==8 || i==5 || i==11)){
+                floorList[i]=new floor();
+            }
+        }
+        floorList[2]=new elevator();
+        floorList[5]= new snake();
+        floorList[8]=new ladder();
+        floorList[11]= new kingCobra();
+        dice d= new dice(2);
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter the player name and hit enter");
+        String name=sc.nextLine();
+        player user=new player(name);
+        System.out.println("The game setup is ready");
+        while(user.getCurrFloor()!=13){ //condition to be changed later
+            System.out.print("Hit enter to roll the dice");
+            sc.nextLine();
+            d.roll();
+            System.out.println("Dice gave " + d.getFaceValue());
+            if ((user.getCurrFloor()==-1)&&(user.getCurrFloor()+d.getFaceValue()!=0)){
+                System.out.println("Game cannot start until you get 1");
+            }
+            else if (user.getCurrFloor()+d.getFaceValue()>13){
+                System.out.println("Player cannot move");
+            }
+            else {
+                user.setCurrFloor(user.getCurrFloor()+d.getFaceValue());
+                floorList[user.getCurrFloor()].update(user);
+            }
+        }
+        System.out.println("Game Over");
+        System.out.println(user.getName() + " accumulated " + user.getPoint() + " points.");
+    }
+}
